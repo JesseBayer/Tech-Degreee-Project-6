56813e9f212ab5e4c02fee81ec001eab16dd44c1
Jesse Bayer <jessebayer53@gmail.com>
2022-07-07 18:22:50 -0500

    Added phrases, get random phrase, and add phrase to display

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
 
+//Hide's the start screen
 const start = document.querySelector(".btn__reset");
 const startScreen = document.getElementById('overlay');
 
@@ -11,5 +12,40 @@ start.addEventListener('click', () => {
 });
 
 
+const phrases = [
+    'Boulevard of Broken Dreams',
+    'Welcome to the Black Parade',
+    'Dear Maria Count me in',
+    'I Write Sins not Tragedies',
+    'Teenage Dirtbag'
+];
 
+//Gets a random phrase from the phrases array
+function getRandomPhraseAsArray(arr) {
+    const random = arr[Math.floor(Math.random() * arr.length)];
+    return random.split("");
+};
 
+//Assigns the phrases array item to a variable
+let randomPhrase = getRandomPhraseAsArray(phrases);
+
+//Adds array item to display
+function addPhraseToDisplay(arr) {
+    const ul = document.querySelector("#phrase");
+
+    for (i = 0; i < arr.length; i++) {
+        const li = document.createElement('li');
+        li.textContent = arr[i];
+        ul.appendChild(li);
+
+        if (arr[i].textContent === " "){
+            li.classList.add("space");
+        } else{
+            li.classList.add("letter");
+        }
+
+    }
+
+};
+
+addPhraseToDisplay(randomPhrase);
