11144879b43c15389a907505a7bce633453a28a4
Jesse Bayer <jessebayer53@gmail.com>
2022-07-07 02:34:04 -0500

    First few steps done

---
+++ app.js
@@ -0,0 +1,15 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+let missed = 0;
+
+const start = document.querySelector(".btn__reset");
+const startScreen = document.getElementById('overlay');
+
+start.addEventListener('click', () => {
+    
+    startScreen.style.display = "none";
+});
+
+
+
+
