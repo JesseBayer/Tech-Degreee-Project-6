ae72afccbeaef3b2e09fb1767ba9c1d924a58da5
Jesse Bayer <jessebayer53@gmail.com>
2022-07-21 16:30:45 -0500

    First review version

--- app.js
+++ app.js
@@ -49,19 +49,22 @@ function addPhraseToDisplay(arr) {
 
 addPhraseToDisplay(randomPhrase);
 
+//Function to check if the letter clicked on the on screen keyboard in in the phrase
 function checkLetter(btn) {
     let letter = document.querySelectorAll('li');
     let match = null;
     for (i=0; i < letter.length; i++) {
         if (btn.textContent === letter[i].textContent) {
             letter[i].classList.add("show");
-            match = btn.textContent;
+            match = btn.textContent;   
         } 
     }
-
     return match;
 };
 
+
+//Event listener to add the chosen class to the buttons on screen 
+//and change the hearts to lost hearts if the wrong letter is chosen
 qwerty.addEventListener('click', (e) => {
     let btn = e.target;
     if (e.target.tagName === 'BUTTON' && e.target.tagName != "chosen") {
@@ -69,13 +72,46 @@ qwerty.addEventListener('click', (e) => {
     }
     const letterFound = checkLetter(btn);
 
-    if(letterFound === null ) {
-        const tries = document.querySelectorAll('.tries');
-        const triesArray = Array.from(tries);
-        for (i = 0; i < triesArray.length; i++) {
-            triesArray[i].style.display = 'none';
-        }
+    if(letterFound === null && e.target.tagName === 'BUTTON') {
+        const tries = document.querySelectorAll("img");
+		tries[missed].setAttribute("src", "images/lostHeart.png");
+		missed++;
     }
+    checkWin();
 });
 
+//Function that checks for a win
+let letters = document.getElementsByClassName('letter');
+let shows = document.getElementsByClassName('show');
+
+function checkWin() {
+    if (letters.length === shows.length) {
+        //Added Timeout function because I thought the overlay change was jarring
+        setTimeout(() => {
+            startScreen.className = "win",
+            startScreen.children[0].textContent = "You Win!",
+            start.textContent = "Reset",
+            startScreen.style.display = "flex"
+          }, 500)
+        //Changes the start button in the win/lose screen to a reset button 
+        //that refreshes the page
+        start.addEventListener('click', () => {
+            window.location.reload();
+        });
+
+    } else if (missed >= 5) {
+        setTimeout(() => {
+            startScreen.className = "lose",
+            startScreen.children[0].textContent = "Try Again",
+            start.textContent = "Reset",
+            startScreen.style.display = "flex"
+          }, 500)
+
+        start.addEventListener('click', () => {
+            window.location.reload();
+        });
+    }
+
+   
+}
 
